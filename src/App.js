9384c87b96e840cda791a9869716885a908bc3c8
andrey <shmigelskiyandrey@gmail.com>
2023-02-17 19:45:02 +0400

    add header layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const zk = 5;
+  const uni = 10;
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div>
+        <h1>Exchange calculator</h1>
+        <div className="first">
+          <div>
+            <input name="currency" list="currency" />
+            <datalist id="currency">
+              <option value="RUB" />
+              <option value="USD" />
+              <option value="EUR" />
+              <option value="LAR" />
+            </datalist>
+          </div>
+          <div className="first-between">
+            <p className="first-text">to</p>
+            <img
+              src="./images/Arrow1.png"
+              alt=" "
+              className="first-arrow"
+            ></img>
+          </div>
+          <div>
+            <input name="currency" list="currency" />
+            <datalist id="currency">
+              <option value="RUB" />
+              <option value="USD" />
+              <option value="EUR" />
+              <option value="LAR" />
+            </datalist>
+          </div>
+        </div>
+        <div>
+          <input></input>
+        </div>
+        <div className="second">
+          <div className="zk">
+            <p>{`Золотая корона: ${zk}`}</p>
+            <output></output>
+          </div>
+          <div className="uni">
+            <p>{`Unistream: ${uni}`}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
